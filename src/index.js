a17b70207bb00852e861b05c1ec0e8c330816e7f
Mathias Denichi <mathias.denise@ochtec.com>
2019-02-10 18:38:34 -0500

    Added foor button and localforage.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YouTube from "react-youtube";
-import { Card, CardTitle } from "react-md";
-
-import "./styles.css";
+import localforage from 'localforage'
+import { Card, CardTitle, Button } from "react-md";
 
 class App extends Component {
+  state = {
+    buttonPress: 0,
+  }
+
+  componentDidMount = () => {
+    this.loadLocal()
+  }
+
+  loadLocal = async () => {
+    const buttonPress = await localforage.getItem('presses') || 0
+
+    console.log(buttonPress, 'data')
+    return this.setState({
+      buttonPress,
+    })
+  }
+
+  setLocal = async () => {
+    const { buttonPress } = this.state
+    return localforage.setItem('presses',  buttonPress + 1 )
+  }
+
+  handleAdd = () => {
+    const { buttonPress } = this.state
+    this.setState({
+      buttonPress: buttonPress + 1,
+    })
+    this.setLocal()
+  }
+
   render() {
     const options = {
       height: "390",
@@ -17,6 +46,9 @@ class App extends Component {
         loop: 1
       }
     };
+
+    const { buttonPress } = this.state
+    console.log('Look who learned to code!!!!!!')
     return (
       <Card>
         <CardTitle
@@ -24,6 +56,16 @@ class App extends Component {
           className="md-grid md-cell md-cell--6"
         />
         <YouTube opts={options} videoId="Wb-Sj3HO5WM" />
+        <Button
+          name='footButton'
+          id='footButton'
+          primary
+          raised
+          onClick={this.handleAdd}
+          style={{ color: 'white', background: 'grey', padding: 10, fontSize: 27 }}
+          >
+          {`You've Pressed the HAIL FOOT LORD button ${buttonPress} times`}
+          </Button>
       </Card>
     );
   }
