282687ebb1d03fd0089b183e015b4b0eb48dc672
Mathias Denichi <mathias.denise@ochtec.com>
2019-02-15 19:57:24 -0500

    Port 5000

--- src/server/index.js
+++ src/server/index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.use(express.static('dist'))
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`))
+app.listen(process.env.PORT || 5000, () => console.log(`Listening on port ${process.env.PORT || 5000}!`))
